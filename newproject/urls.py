5b85cd4d03bfafb13e24179da7864bde299b479c
Quratulain983 <quratulain@mexemai.com>
2026-06-10 16:26:22 +0500

    modify reqs

--- newproject/urls.py
+++ newproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
-from newpojectr.views import health_check 
+from newpoject.views import health_check 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
